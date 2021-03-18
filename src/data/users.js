f3a14ab19d7fa207d28417552f82d23fd464a51d
Jacek <jacek.olszowski1@gmail.com>
2021-03-18 19:09:08 +0100

    add UserList component

---
+++ src/data/users.js
@@ -0,0 +1,22 @@
+export const users = [
+  {
+    name: 'Jacek Olszowski',
+    attendance: '39%',
+    average: '2.3',
+  },
+  {
+    name: 'Agnieszka Urtnowska',
+    attendance: '58%',
+    average: '4.3',
+  },
+  {
+    name: 'Jan Nowak',
+    attendance: '52%',
+    average: '3.8',
+  },
+  {
+    name: 'Harry Potter',
+    attendance: '72%',
+    average: '4.1',
+  },
+];
